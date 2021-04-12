31a7050bc9cb2b195eea9bb8177d1e7be5a5868b
Victoria9307 <80662688+Victoria9307@users.noreply.github.com>
2021-04-12 20:34:30 +0900

    Create loop_practice.1.py

---
+++ loop_practice.1.py
@@ -0,0 +1,14 @@
+# Q.1 for 문을 이용하여 1부터 입력받은 정수까지의 합을 구하여 출력하는 프로그램을 작성하시오.¶
+# 100 이하의 양의 정수만 입력된다.
+# 입력 예: 10
+# 출력 예: 55
+
+number = int(input("Number Please : "))
+sum = 0
+for x in range (1, number+1) :
+    sum += x
+if number <= 100 :
+    print(sum)
+    
+    
+ 
